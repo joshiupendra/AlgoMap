d545f34809695eab05e54aa29000975a3aa5c285
Upendra Joshi <joshiupendra9@gmail.com>
2024-09-03 12:13:29 +0530

    Trapping Rain Water

--- src/twoPointers/TrappingRainWater.java
+++ src/twoPointers/TrappingRainWater.java
@@ -14,51 +14,46 @@ In this case, 6 units of rain water (blue section) are being trapped.
 Example 2:
 Input: height = [4,2,0,3,2,5]
 Output: 9
+
+Example 3:
+Input: height = [6,4,2,0,3,2,0,3,1,4,5,3,2,7,5,3,0,1,2,1,3,4,6,8,1,3]
+Output: 83
  */
 
 public class TrappingRainWater {
 
 	public static void main(String[] args) {
-		int[] height = {4,2,0,3,2,5};
+		int[] height = { 6, 4, 2, 0, 3, 2, 0, 3, 1, 4, 5, 3, 2, 7, 5, 3, 0, 1, 2, 1, 3, 4, 6, 8, 1, 3 };
 		int result = trap(height);
 		System.out.println(result);
 	}
 
 	public static int trap(int[] height) {
-        int result = 0;
-        if (height.length < 2) {
-        	return result;
-        }
-        
-        int[] water = new int[height.length];
-        int right = height[0], left = 0;
-        water[0] = 0;
-        
-        for (int i=1; i<height.length; i++) {
-        	int k = 0;
-        	int j = height.length - 1;
-        	right = height[0];
-        	left = height[j];
-        	while (k < i || j > i) {
-        		if (k < i) {
-        			right = Math.max(height[k], right);
-        			k++;
-        		}
-        		if (j > i) {
-        			left = Math.max(height[j], left);
-        			j--;
-        		}
-        		int temp = Math.min(right, left);
-        		water[i] = (temp > height[i])? temp - height[i] : 0;
-        		
-        	}
-        	//System.out.println(i + " " + height[i] + " " + right + " " + left + " " + water[i]);
-        }
-        for (int w: water) {
-        	//System.out.print(w + " ");
-        	result += w;
-        }
-        
-        return result;
-    }
+		int result = 0;
+		int n = height.length;
+		int lWall = 0, rWall = 0;
+		int[] maxLeft = new int[n];
+		int[] maxRight = new int[n];
+
+		int j = 0;
+		for (int i = 0; i < n; i++) {
+			j = n - i - 1;
+			maxLeft[i] = lWall;
+			maxRight[j] = rWall;
+			lWall = Math.max(lWall, height[i]);
+			rWall = Math.max(rWall, height[j]);
+		}
+
+		int[] water = new int[n];
+		for (int i = 0; i < n; i++) {
+			int potential = Math.min(maxLeft[i], maxRight[i]);
+			water[i] = Math.max(0, potential - height[i]);
+		}
+
+		for (int w: water) { 
+			result += w; 
+		}
+
+		return result;
+	}
 }
